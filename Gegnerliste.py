39be9611797a747008826fc62313089e1d22bfb9
MQuaing <144004308+MQuaing@users.noreply.github.com>
2023-10-20 09:44:00 +0200

    neu

---
+++ Gegnerliste.py
@@ -0,0 +1,10 @@
+from Bilder import * 
+
+
+# Verschiedne Gegner
+Malikar = [300, 20, 30, 40,]
+Varoth = [150, 15, 20, 30]
+Zyrelia = [200, 10, 15, 25]
+Malkor = [175, 5, 15, 40]
+Dreznar = [150, 10, 20, 20]
+Vorlunda = [170, 10, 20, 30,Vorluna_Bild]
