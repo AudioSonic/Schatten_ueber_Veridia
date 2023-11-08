77fbc731686ca3d4a1bdcb71b421d3cc9095dbd2
AudioSonic <79091924+AudioSonic@users.noreply.github.com>
2023-11-08 11:45:58 +0100

    Höhle Feld 2,2 Text eingefügt

--- Fight.py
+++ Fight.py
@@ -402,7 +402,7 @@ def Fight(root, parent_frame, gegner):
     
     # Erstelle eine Variable zur Verfolgung der Boss-HP
     boss_hp = tk.IntVar()
-    boss_hp.set(10) # Starte mit vollen HP
+    boss_hp.set(100) # Starte mit vollen HP
     
     # Erstelle einen determinierten Ladebalken f�r die Boss-HP
     progress_bar = ttk.Progressbar(root, mode="determinate", variable=boss_hp, length=300)
@@ -411,43 +411,35 @@ def Fight(root, parent_frame, gegner):
     #Setzt die Breite der Buttons fest
     button_width = 12
     
-
     # Button f�r den Spielerangriff
     attack_button = ttk.Button(root, text="Attack", padding=(50, 10), width= button_width, style="TNR.TLabel", command=attack)
     attack_button.place(x=43, y=680)
-
 
     #Der "Recover" Button
     recover_button = ttk.Button(root, text="Recover", padding=(50, 10), width=button_width, style="TNR.TLabel", command=recover)
     recover_button.place(x=293, y=680)
 
-
     #Der "Counter" Button
     counter_button = ttk.Button(root, text="Counter", padding=(50, 10), width= button_width, style="TNR.TLabel", command=counter)
     counter_button.place(x=543, y=680)
     
-
     #Der "Inventory" Button
     potion_button = ttk.Button(root, text= "Potion", padding=(50, 10), width= button_width, style="TNR.TLabel", command=potion)
     potion_button.place(x=793, y=680)
     
-    
-
     #Der "Escape" Button
     escape_button = ttk.Button(root, text= "Escape", padding=(50, 10), width=button_width, style="TNR.TLabel", command=escape)
     escape_button.place(x=1043, y=680)
-
 
     # Player View
     Player_output = tk.Text(root, height=3, width=25, wrap="none")
     Player_output.place(x = 50, y = 375)
     Player_output.config(state=tk.DISABLED)  # Sperrt das Textfeld
     
-
     #Bearbeitung Name/HP/Stamina
     show_HP =  f"HP:                  {player_hp.get()}/120"
     show_STM = f"Stamina:          {player_STM.get()}/30"
-    player =   "Max"
+    player =   "Servitus"
 
     Player_output.config(state=tk.NORMAL)
     Player_output.insert(tk.END, f"   {player}\n   {show_HP}\n   {show_STM}")
@@ -455,22 +447,18 @@ def Fight(root, parent_frame, gegner):
     custom_font = ("Times New Roman", 16)
     Player_output.config(font=custom_font)
 
-    
     # Textfeld fuer die Ausgabe
     text_output = tk.Text(root, height=6, width=90, wrap="none")
     text_output.place(x = 18, y = 470)
     text_output.config(state=tk.DISABLED)  # Sperrt das Textfeld
-
 
     #Einfuegen und aendern des Lauftext
     text_output.config(state=tk.NORMAL)
     text_output.insert(tk.END, gegner[5])
     text_output.config(state=tk.DISABLED)      
 
-
     custom_font = ("Times New Roman", 20)
     text_output.config(font=custom_font)
-
 
     root.mainloop()
     return root
